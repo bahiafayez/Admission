6f4887b27a6121e2cf06d1f2d0448f550a74597f
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2012-03-01 22:02:13 +0200

    SkipFilter

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,7 +6,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Admission"
-
+  config.skip_before_filter :authorize
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
