a670be765d4e15d645121759fade252889987a53
ismailsobhy <ismailsobhy@gmail.com>
2012-03-19 02:14:37 +0200

    address unit test

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -3,40 +3,51 @@ require 'test_helper'
 class AddressTest < ActiveSupport::TestCase
    fixtures :addresses
    
-    
+   test "should create address" do
+    address = Address.new
+    address = addresses(:good)
+    assert address.save
+  end
+
+  test "should find address" do
+    address_id = addresses(:good).id
+    assert_nothing_raised { Address.find(address_id) }
+  end
+
+  test "should update address" do
+     address = addresses(:good)
+     assert address.update_attributes(:address => 'Donald home')
+  end
+  
+  test "should destroy address" do
+    address = addresses(:good)
+    address.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Address.find(address.id) }
+  end
+  
    test "address: attributes must not be empty" do
     address = Address.new
     assert address.invalid?
     assert address.errors[:address].any?
-    #assert address.errors[:email].any?
-    
-    #check if an error assiocated with individual input if empty
-    assert address.errors[:telephone].any?
-    
-    #check if an error assiocated with individual input if empty
-    assert address.errors[:mobile].any?
-    
-    #fax is not a must
-    #assert address.errors[:fax].any?
-    
     assert address.errors[:address_type].any?
    end
   #######################address##########################3
-  #test "address: address length" do
+  
+  test "address: address length" do
                      
-  #  address2 = addresses(:address2)          
-  #  if address2.address.length>100
-  #    flunk("Address more than 100 charchters")
-  #  end
+    address2 = addresses(:address)          
+    if address2.address.length>100
+      flunk("Address more than 100 charchters")
+    end
       
-  #end
+  end
   
   #######################email##########################3
   #test "address: email format" do
   #  address = addresses(:one) 
   #  address.email="bob"                    
   #  assert address.invalid?
-  #  assert_equal "Invalid E-mail",address.errors[:email].join('; ')
+  #   assert_equal "Invalid E-mail",address.errors[:email].join('; ')
   #end
   
   #test "address: email should be unique" do
@@ -50,61 +61,70 @@ class AddressTest < ActiveSupport::TestCase
   #  assert address2.invalid?,"mail should not be case sensative"
   #end
   ######################phone############################### 
-  #test "address: phone format" do
-  #  one = addresses(:one)          
-  #  one.telephone="b"
-  #  one.invalid?                
-  #  assert one.invalid?
-  #  assert_equal "Invalid Number",one.errors[:telephone].join('; ')
-  #end
   
-  #test "address: phone length" do
-  #  one = addresses(:one)          
-  #  one.telephone="13623463128725823"               
-  #  assert one.invalid?
-  #  assert_equal "Invalid Number",one.errors[:telephone].join('; ')
-  #end
+  test "address: telephone must exist if mobil empty" do
+    good = addresses(:good)           
+    good.telephone=nil
+    good.mobile=nil
+    assert good.invalid?                      
+    assert_equal "can't be blank",good.errors[:telephone].join('; ')
+  end
+  test "address: phone format" do
+    one = addresses(:address)                      
+    assert one.invalid?
+    assert_equal "Invalid Number",one.errors[:telephone].join('; ')
+  end
+  
+  test "address: phone length" do
+    one = addresses(:address2)                       
+    assert one.invalid?
+    assert_equal "Invalid Number",one.errors[:telephone].join('; ')
+  end
   ########################mobile#################################
-  #test "address: mobile format" do
+  test "address: mobile format" do
     
-  #  one = addresses(:one)          
-  #  one.mobile="b"
-  #  assert one.invalid?
-  #  assert_equal "Invalid Number",one.errors[:mobile].join('; ')
-  #end
+    one = addresses(:address)          
+    assert one.invalid?
+    assert_equal "Invalid Number",one.errors[:mobile].join('; ')
+  end
   
-  #test "address: mobile length" do
+  test "address: mobile length" do
                      
-  #  one = addresses(:one)          
-  #  one.mobile="124125132612631361"               
-  #  assert one.invalid?
-  #  assert_equal "Invalid Number",one.errors[:mobile].join('; ')
-  #end
+    one = addresses(:address2)
+    one.mobile="212315412632609324760970176"
+    assert one.invalid?
+    assert_equal "Invalid Number",one.errors[:mobile].join('; ')
+  end
   
+  test "address: mobile must exist if telephone empty" do
+    good = addresses(:good)           
+    good.telephone=nil
+    good.mobile=nil
+    assert good.invalid?                      
+    assert_equal "can't be blank",good.errors[:mobile].join('; ')
+  end
   #########################fax###############################
-  #test "address: fax format" do
-  #  one = addresses(:one)          
-  #  one.fax="b"
-                
-  #  assert one.invalid?
-  #  assert_equal "Invalid Number",one.errors[:fax].join('; ')
-  #end
+  test "address: fax format" do
+    one = addresses(:address)              
+    assert one.invalid?
+    assert_equal "Invalid Number",one.errors[:fax].join('; ')
+  end
   
-  #test "address: fax length" do
+  test "address: fax length" do
           
-  #  one = addresses(:one)          
-  #  one.fax="212315412632609324760970176"               
-  #  assert one.invalid?
-  #  assert_equal "Invalid Number",one.errors[:fax].join('; ')
-  #end
+    one = addresses(:address2)          
+    one.fax="212315412632609324760970176"               
+    assert one.invalid?
+    assert_equal "Invalid Number",one.errors[:fax].join('; ')
+  end
   ##########################address type######################################
-  #test "address type" do
+  test "address type" do
     
-  #  address2 = addresses(:address2)         
-  #  address2.address_type="clinic"
-  #  assert address2.invalid?
-  #  assert_equal "is not included in the list", address2.errors[:address_type].join('; ')
-  #end 
+    address2 = addresses(:good)         
+    address2.address_type="clinic"
+    assert address2.invalid?
+    assert_equal "is not included in the list", address2.errors[:address_type].join('; ')
+  end 
   ####################################################################### 
 
   
