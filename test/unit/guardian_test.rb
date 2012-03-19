c2801df26f0646ee8ef99c73466b7c5fc4d20c65
ismailsobhy <ismailsobhy@gmail.com>
2012-03-19 17:40:48 +0200

    unit test for guardian

--- test/unit/guardian_test.rb
+++ test/unit/guardian_test.rb
@@ -3,6 +3,30 @@ require 'test_helper'
 class GuardianTest < ActiveSupport::TestCase
   
   fixtures :guardians
+  
+  test "should create guradian" do
+    guardian = Guardian.new
+    guardian = guardians(:good)
+    assert guardian.save
+  end
+
+  test "should find guradian" do
+    guardian_id = guardians(:good).id
+    assert_nothing_raised { Guardian.find(guardian_id) }
+  end
+
+  test "should update guradian" do
+     guardian = guardians(:good)
+     assert guardian.update_attributes(:first_name => 'any')
+  end
+  
+  test "should destroy guradian" do
+    guardian = guardians(:good)
+    guardian.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Guardian.find(guardian.id) }
+  end
+  
+  
   setup do  
       @guardian1 = guardians(:guardian1)  
    end 
@@ -12,41 +36,30 @@ class GuardianTest < ActiveSupport::TestCase
    end 
   test "guardian attributes must not be empty" do
     guardian = Guardian.new
-   
     assert guardian.invalid?
     assert guardian.errors[:first_name].any?
-    #assert guardian.errors[:middle_name].any?
     assert guardian.errors[:last_name].any?
     assert guardian.errors[:address].any?
-    assert guardian.errors[:telephone].any?
-    assert guardian.errors[:mobile].any?
     assert guardian.errors[:occupation].any?
-    assert guardian.errors[:company_name].any?
-    assert guardian.errors[:business_telephone].any?
-    assert guardian.errors[:email].any?
-    assert guardian.errors[:fax].any?
-    assert guardian.errors[:business_address].any?
    end
   
   
   ###############################first name##########################################
   
   test "address first name should be alphbats" do
-    guardian1 = guardians(:guardian1)                       
-    guardian1.first_name="-As"
-    assert_match( /^[a-zA-Z]+$/, guardian1.first_name, ["first name contains non alphabetical charchters"] )
+    assert @guardian2.invalid?
+    assert_equal "first name contains non alphabetical characters",@guardian2.errors[:first_name].join('; ')
   end
   ###############################middle name##########################################
   test "address middle name should be alphbats" do
-    guardian1 = guardians(:guardian1)                       
-    guardian1.middle_name="-john"                     
-    assert_match( /^[a-zA-Z]+$/, guardian1.middle_name, ["middle name contains non alphabetical charchters"] )
+    assert @guardian2.invalid?
+    assert_equal "middle name contains non alphabetical characters",@guardian2.errors[:middle_name].join('; ')
   end
   ################################last name#########################################
   test "address last name should be alphbats and no spaces" do
-    guardian1 = guardians(:guardian1)                       
-    guardian1.last_name="122jack"                     
-    assert_match( /^[a-zA-Z]+$/, guardian1.last_name, ["last name contains non alphabetical charchters"] )
+     assert @guardian2.invalid?
+    assert_equal "last name contains non alphabetical characters",@guardian2.errors[:last_name].join('; ')
+
   end
   ##############################address##############################################
   test "address: address length" do
@@ -61,55 +74,82 @@ class GuardianTest < ActiveSupport::TestCase
   #############################telephone##########################################
   test "is guradian's phone valid?" do
                      
-    guardian1 = guardians(:guardian1)
-    guardian1.telephone="-A123"
-    assert_match( /[a-zA-Z]+$/, guardian1.telephone, ["phone contains alphabetical charchters"] )
+    assert @guardian1.invalid?
+    assert_equal "Invalid Number",@guardian1.errors[:telephone].join('; ')
+  end
+  
+  test "phone must exist if mobile empty" do
+    good = guardians(:good)           
+    good.telephone=nil
+    good.mobile=nil
+    assert good.invalid?                      
+    assert_equal "can't be blank",good.errors[:mobile].join('; ')
   end
   #############################mobile##########################################
   test "is mobile format valid?" do
                      
-    guardian1 = guardians(:guardian1)
-    guardian1.mobile="-A123" 
-    assert_match( /[a-zA-Z]+$/, guardian1.mobile, ["mobile contains alphabetical charchters"] )
+    assert @guardian1.invalid?
+    assert_equal "Invalid Number",@guardian1.errors[:mobile].join('; ')
   end
   
+  test "mobile must exist if phone empty" do
+    good = guardians(:good)           
+    good.telephone=nil
+    good.mobile=nil
+    assert good.invalid?                      
+    assert_equal "can't be blank",good.errors[:phone].join('; ')
+  end
    ##############################occupation##############################################
   test "business occupation should be alphbats" do
-    guardian1 = guardians(:guardian1)                       
-    guardian1.occupation="122doctor"                     
-    assert_match( /^[a-zA-Z]+$/, guardian1.occupation, ["address contains non alphabetical charchters"] )
+                                  
+    assert_equal "Invalid Number",@guardian1.errors[:ocupation].join('; ')
   end
   
   ######################business telephone###################################
   test "business telephone valid" do
-    guardian1 = guardians(:guardian1)
-    guardian1.business_telephone="-A123"                 
-    assert_match( /[a-zA-Z]+$/, guardian1.business_telephone, ["business phone contains alphabetical charchters"] )
+    assert @guardian1.invalid?
+    assert_equal "Invalid Number",@guardian1.errors[:business_telephone].join('; ')
   end
   
+  test "business phone must exist if email empty" do
+    good = guardians(:good)           
+    good.business_telephone=nil
+    good.email=nil
+    assert good.invalid?                      
+    assert_equal "can't be blank",good.errors[:business_telephone].join('; ')
+  end
   ######################fax###################################
   test "is fax format valid?" do
-    guardian1 = guardians(:guardian1)
-    guardian1.fax="-A123"                 
-    assert_match( /^[a-zA-Z]+$/, guardian1.fax, ["fax contains alphabetical charchters"] )
+    assert @guardian1.invalid?
+    assert_equal "Invalid Number",@guardian1.errors[:fax].join('; ')
   end
   
   
   ######################email###################################
   test "address: is email valid" do
-    guardian1 = guardians(:guardian1)
-    guardian1.email="nomail"                 
-    assert guardian1.invalid?
-    assert_equal "Invalid E-mail",guardian1.errors[:email].join('; ')
+    assert @guardian1.invalid?
+    assert_equal "Invalid E-mail",@guardian1.errors[:email].join('; ')
   end
   
+   test "business phone can be empty if email exist" do
+    good = guardians(:good)           
+    good.business_telephone=nil
+    good.email=nil
+    assert good.invalid?                      
+    assert_equal "can't be blank",good.errors[:business_telephone].join('; ')
+  end
   #####################business_address################################## 
   test "address: business_address length" do
                      
-   guardian = guardians(:guardian2)            
-    if guardian.business_address.length>100
+              
+    if @guardian2.business_address.length>100
       flunk("Address more than 100 charchters")
     end   
   end  
-
+  
+  test "applicant's country of issuance should be a valid place" do
+    good = guardians(:good)          
+    good.financial="anything"
+    assert_equal "is not included in the list",good.errors[:financial].join('; ')                     
+  end
 end
