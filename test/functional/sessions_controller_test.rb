429157a2c9484050de53b4bd464f6d9f6494ec67
ismailsobhy <ismailsobhy@gmail.com>
2012-03-21 17:08:37 +0200

    session functionl testing+ unit testing of user,semifinal

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,9 +1,38 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
+  
+  setup do
+    @user = users(:good)
   end
+  
+  #??
+  test "should create user, but goes to new" do
+    post :create, { :email => 'mail@gmail.com',:password =>'123qwe' }
+    assert_response :success
+   assert_equal "Invalid email or password1", flash[:notice]
+  end
+  
+  
+  test "sessions: already logged in" do
+    @request.session[:user_id] = users("one").id
+    get :new
+    assert_response :redirect
+    assert_equal "#{users('one').email} is already logged in, Please Log out first", flash[:notice]
+  end
+  
+  
+  test "should destroy sessions" do
+    
+    delete :destroy, :id => @user.to_param
+    assert_response :redirect
+    assert_redirected_to root_path
+
+    assert_equal "Logged out!", flash[:notice]
+    
+    assert_raise(ActiveRecord::RecordNotFound) { User.find(@user.to_param) }
+
+    end
+
 
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+  test "User must not be empty" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:email].any?
+    assert user.errors[:password].any?
+
+   end
+   
+   test "users: password max length" do
+    good = users(:good) 
+    good.password= "123aaaaaaaaaaassssssssssssssssssssssssss111111111111111"           
+    assert good.invalid?
+    assert_equal "is too long (maximum is 40 characters)",good.errors[:password].join('; ')   
+  end 
+  
+  test "users: password min length" do
+    good = users(:good) 
+    good.password= "1"           
+    assert good.invalid?
+    assert_equal "is too short (minimum is 6 characters)",good.errors[:password].join('; ')   
+  end  
+  
+  test "user email must be valid" do
+     one = users(:one) 
+     assert one.invalid?
+     assert_equal "Invalid E-mail",one.errors[:email].join('; ')
+  end
+  
+  #test "user email unique" do
+                         
+  #   good= users(:good) 
+  #   assert !good.save
+  #   assert_equal 'email has already taken', good.errors[:email]
+  #end
+  
+  
+  
 end
