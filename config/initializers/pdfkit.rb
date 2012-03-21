48ffd1bb3f013da55e2c1af5febc85c9ba29317c
Omnia Zayed <omnia.zayed@gmail.com>
2012-03-21 18:56:43 +0200

    omnia design-footer modification in home

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,14 +1,12 @@
 # config/initializers/pdfkit.rb  
   
   
-PDFKit.configure do |config|       
-  
-  
-     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env.production?  
-  
-  
-end  
-
+#PDFKit.configure do |config|          
+#     config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env.production?     
+#end  
+PDFKit.configure do |config|
+    config.wkhtmltopdf = 'C:/wkhtmltopdf/wkhtmltopdf.exe'
+  end
 WICKED_PDF = {
 :exe_path => '/bin'
 }
