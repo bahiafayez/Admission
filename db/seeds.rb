12955f40fe74a4cacfb92fd003026fa81c84e057
ismailsobhy <ismailsobhy@gmail.com>
2012-05-05 20:41:26 +0200

    moved default user from migration to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create a default user
+AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :role => 'Admission')
+
